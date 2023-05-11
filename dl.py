f91ab62a0a7a13656b571fe67ea2d0e1f2fc3b81
p4ul17 <86480417+p4ul17@users.noreply.github.com>
2023-05-11 08:48:18 +0200

    Added extended Support for embedded Videos

--- dl.py
+++ dl.py
@@ -29,6 +29,9 @@ def main():
         download(URL)
 
 def help():
+    print("Version v1.1.0")
+    print("")
+    print("______________")
     print("Arguments:")
     print("-h shows this help")
     print("-u <URL> downloads the <URL> you specify")
@@ -47,21 +50,23 @@ def list_dl(doc):
 
 def download(URL):
     URL = str(URL)
-    URL = URL.replace("/e/", "/") # replaces /e in the URL with an empty String
-                                # to make dowloading an embedded URL possible
 
     html_page = requests.get(URL)
     soup = BeautifulSoup(html_page.content, 'html.parser')
 
-    name_find = soup.find("h1",class_="mt-1")  #parsing h1 tag for name of the mp4 file
-    name = name_find.text
+    name_find = soup.find("title").text
+    slice_start = name_find.index("Watch ") + 6
+    name = name_find[slice_start:]
+    slice_end = name.index(" - VOE")
+    name = name[:slice_end]
     name = name.replace(" ","_")
+    print(name)
 
     sources_find = soup.find_all(string = re.compile("var sources")) #searching for the script tag containing the link to the mp4
     sources_find = str(sources_find)
     #slice_start = sources_find.index("const sources")
     slice_start = sources_find.index("var sources")
-    source = sources_find[slice_start:] #cutting everything before 'const sources' in the script tag
+    source = sources_find[slice_start:] #cutting everything before 'var sources' in the script tag
     slice_end = source.index(";")
     source = source[:slice_end] #cutting everything after ';' in the remaining String to make it ready for the JSON parser
 
@@ -82,8 +87,7 @@ def download(URL):
     except KeyError:
         try:
             link = source_json["hls"]
-            name = name +'SS.mp4'
-            print(name)
+            name = name +'_SS.mp4'
 
             ydl_opts = {'outtmpl' : name,}
             with YoutubeDL(ydl_opts) as ydl:
@@ -91,7 +95,7 @@ def download(URL):
             delpartfiles()
 
         except KeyError:
-            print("Could not find downloadable URL. Voe might have change their site. Check that you are running the latest version of voe-dl, and i so file an issue on GitHub.")
+            print("Could not find downloadable URL. Voe might have change their site. Check that you are running the latest version of voe-dl, and if so file an issue on GitHub.")
             quit()
     
     print("\n")
