fe87c22a39db3c58b297896c9cbcb254faf1c267
Niklas S <nikoverflow007@gmail.com>
2024-05-13 16:02:38 +0200

    fix: fix that the mp4 source can be found.

--- dl.py
+++ dl.py
@@ -51,7 +51,7 @@ def list_dl(doc):
 def download(URL):
     URL = str(URL)
 
-    html_page = requests.get(URL)
+    html_page = requests.get(URL, headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"})
     soup = BeautifulSoup(html_page.content, 'html.parser')
 
     name_find = soup.find("title").text
